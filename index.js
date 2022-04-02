ec284fe72f0e9519f09d23c98a6f334df965cc9a
Dark <istiak.csinfo@gmail.com>
2022-04-02 23:36:15 +0600

    new

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
-document.querySelector('.AddTaskBtn').addEventListener('click', e => {
+// Project: TaskLIST
+import UI from './UI.js'
+import Task from './Task.js'
+const ui = new UI();
+document.querySelector('.AddTaskBtn').addEventListener('click', (e) => {
     const taskTitle = document.querySelector('#newtaskID').value
     console.log(taskTitle);
-    const task = new Task(taskTitle)
+    const task = new Task(taskTitle);
 
+    ui.addToUI(task);
+    ui.resetForm();
     console.log(task);
 })
