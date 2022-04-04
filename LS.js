5b799ca291ff7c5f7fbdcafdd00249387d4245e7
Dark <istiak.csinfo@gmail.com>
2022-04-04 23:58:14 +0600

    up

--- LS.js
+++ LS.js
@@ -1,3 +1,18 @@
 function LS() {}
 
+LS.prototype.fetchTask = function() {
+    let tasks = localStorage.getItem('tasks');
+    if (tasks) {
+        tasks = JSON.parse(task)
+    } else {
+        tasks = [];
+    }
+    return tasks;
+}
+LS.prototype.storeTask = function(task) {
+    let tasks = thsi.fetchTask();
+    tasks.unshift(task);
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+}
+
 export default LS();
