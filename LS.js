64d27c08906c03eab4d8cd68c36aee8eea0708fb
Dark <istiak.csinfo@gmail.com>
2022-04-05 00:05:09 +0600

    Update LS.js

--- LS.js
+++ LS.js
@@ -3,16 +3,16 @@ function LS() {}
 LS.prototype.fetchTask = function() {
     let tasks = localStorage.getItem('tasks');
     if (tasks) {
-        tasks = JSON.parse(task)
+        tasks = JSON.parse(tasks)
     } else {
         tasks = [];
     }
     return tasks;
 }
 LS.prototype.storeTask = function(task) {
-    let tasks = thsi.fetchTask();
+    let tasks = this.fetchTask();
     tasks.unshift(task);
     localStorage.setItem('tasks', JSON.stringify(tasks));
 }
 
-export default LS();
+export default LS;
